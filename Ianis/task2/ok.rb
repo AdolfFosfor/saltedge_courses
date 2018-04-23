ba10755219aac408c68b05a0b306679dbc7aa18d
Adolf Fosfor <Adolffosfor@gmail.com>
2018-04-23 12:46:20 +0300

    1

--- Ianis/task2/ok.rb
+++ Ianis/task2/ok.rb
@@ -6,42 +6,43 @@ class Odnoklassniki
   def initialize
     @browser = Watir::Browser.new :chrome
     browser.goto("google.com")
-    sleep(1)
+    page_ready?
   end
 
   def collect_links
-    search_person
-    return unless present_photo?
-    take_photo
-    { links: take_links }
-  end
-
-  def search_person
-    browser.text_field(id: "lst-ib").set("Иван Петров" + " одноклассники")
-    browser.input(name: "btnK").click
-    sleep(1)
-    browser.div(class: "srg").links[0].click
-  end
-
-  def take_photo
-    browser.ul(class: "recent-bar_cnt").links[0].click
-    sleep(2)
-  end
-
-  def take_links
-    photo = []
-    3.times {
-      photo << browser.div(id: "photo-layer_img_w").image.attribute_value("src").to_s
-      browser.span(id: "plsp_next").click
-      sleep(1)
-    }
-    photo.uniq
-  end
-
-  def present_photo?
-    browser.ul(class: "recent-bar_cnt").present?
-  end
+  search_person
+  return unless present_photo?
+  open_photo
+  [ take_links ]
 end
 
+def search_person
+  browser.text_field(id: "lst-ib").set("Иван Петров" + " одноклассники")
+  browser.input(name: "btnK").click
+  browser.div(class: "srg").links[0].click
+end
+
+def open_photo
+  browser.ul(class: "recent-bar_cnt").links[0].click
+  page_ready?
+end
+
+def take_links
+  photo = []
+  3.times {
+    sleep(1)
+    photo << browser.div(id: "photo-layer_img_w").image.attribute_value("src").to_s
+    browser.span(id: "plsp_next").click
+  }
+end
+
+def page_ready?
+  browser.wait unless browser.ready_state.eql? "complete"
+end
+
+def present_photo?
+  browser.ul(class: "recent-bar_cnt").present?
+end
+end
 ok = Odnoklassniki.new
 puts ok.collect_links
